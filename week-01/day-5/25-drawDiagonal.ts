8b84821ccf0fcbf433a3e5653878148cdf4d1c87
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-12 18:48:12 +0200

    Completing the diagonal

---
+++ week-01/day-5/25-drawDiagonal.ts
@@ -0,0 +1,39 @@
+'use strict';
+
+let lineCount: number = 6;
+let percent: string = '%';
+let space: string = ' ';
+let printOut: string = '%';
+
+// Write a program that draws a
+// square like this:
+//
+// %%%%%%
+// %%   %
+// % %  %
+// %  % %
+// %   %%
+// %%%%%%
+//
+// The square should have as many lines as lineCount is
+
+for(let i = 1 ;i <= lineCount ; i++){
+    if(i == 1 || i == lineCount){
+        for(let j = 1; j < lineCount; j++){
+            printOut += percent;
+        };
+        console.log(printOut);
+        printOut = '%';
+    } else if(i > 1 && i < lineCount){
+        for(let k = 1; k < lineCount-1; k++){
+            if(k == i-1){
+                printOut += percent;
+            } else {
+                printOut += space;
+            };
+        };
+        printOut += percent;
+        console.log(printOut);
+        printOut = '%';
+    };
+};
