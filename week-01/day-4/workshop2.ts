3e1f768d9650dd07be41bd60717802f94d414ad6
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-12 14:03:13 +0200

    Completing cuboid

--- week-01/day-4/workshop2.ts
+++ week-01/day-4/workshop2.ts
@@ -97,3 +97,16 @@ if(j % 3 == 0 || j % 5 == 0){
     let check5: boolean = false;
     console.log(check5);
 };
+
+// Cuboid
+
+// Write a program that stores 3 sides of a cuboid as variables (floats)
+// The program should write the surface area and volume of the cuboid like:
+//
+// Surface Area: 600
+// Volume: 1000
+
+let sideLength: number = 10;
+
+console.log('Surface Area: ' + 6 * sideLength ** 2);
+console.log('Volume: ' + sideLength ** 3);
