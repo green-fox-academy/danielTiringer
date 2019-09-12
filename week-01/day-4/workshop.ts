9725ff533cba324fb9eb4a9fa41d74252420432b
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-12 12:05:38 +0200

    Fixed a typo in humpty

--- week-01/day-4/workshop.ts
+++ week-01/day-4/workshop.ts
@@ -10,7 +10,7 @@ console.log('Hello, Daniel, the most promising programmer in the world!');
 
 /* Humpty-dumpty */
 
-console.log('Humpty Dumpty had a great fall.');
 console.log('Humpty Dumpty sat on a wall,');
+console.log('Humpty Dumpty had a great fall.');
 console.log('All the king\'s horses and all the king\'s men');
 console.log('Couldn\'t put Humpty together again.');
