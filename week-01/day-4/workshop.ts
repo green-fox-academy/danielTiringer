774277d29df3d903baaad3817831037fd4a7525e
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-12 13:17:54 +0200

    Swap complete

--- week-01/day-4/workshop.ts
+++ week-01/day-4/workshop.ts
@@ -54,5 +54,18 @@ console.log(dailyCodingTime*workDaysPerWeek/weeklyWorkHours*100)
 
 let favoriteNumber: number = 8;
 
-
 console.log(`My favorite number is: ${favoriteNumber}.`);
+
+// Swap
+
+let a: number = 123;
+let b: number = 526;
+
+let temp: number = a;
+
+a = b;
+
+b = temp;
+
+console.log(a);
+console.log(b);
