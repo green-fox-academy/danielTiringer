71fc844f3123844d2f39f0676cb419055331f56f
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-12 12:05:15 +0200

    Hello World, Hello Me and Humpty Dumpty added

---
+++ week-01/day-4/workshop.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+/* Hello World */
+
+console.log('Hello World!');
+
+/* Hello Me */
+
+console.log('Hello, Daniel, the most promising programmer in the world!');
+
+/* Humpty-dumpty */
+
+console.log('Humpty Dumpty had a great fall.');
+console.log('Humpty Dumpty sat on a wall,');
+console.log('All the king\'s horses and all the king\'s men');
+console.log('Couldn\'t put Humpty together again.');
