06ca1481c461ef4b7f50d13cd186d69fbac2aa63
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-12 16:01:36 +0200

    Completing the parametric average

---
+++ week-01/day-4/19-parametricAverage.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+// Write a program that calculates the sum and the average from 1 to a given number.
+// Example input: 5
+// Example output: Sum: 15, Average: 3
+
+let input: number = 22;
+let sum: number = 0;
+
+for(let i = 1; i <= input; i++){
+    sum += i;
+}
+
+console.log('Sum: ' + sum + ', Average: ' + sum / input);
