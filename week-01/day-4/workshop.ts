0261552455610078d7c78fb8dcba4a6a95662869
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-12 12:20:30 +0200

    Adding the two numbers exercise

--- week-01/day-4/workshop.ts
+++ week-01/day-4/workshop.ts
@@ -26,3 +26,28 @@ console.log('Hello, Kriszti!');
 console.log('Daniel');
 console.log('33');
 console.log('1.80');
+
+// Two numbers
+
+const number1: number = 22;
+const number2: number = 13;
+
+console.log(number1+number2);
+console.log(number1-number2);
+console.log(number1*number2);
+console.log(number1/number2);
+console.log(number1%number2);
+
+
+
+// Create a program that prints a few operations on two numbers: 22 and 13
+
+// Print the result of 13 added to 22
+
+// Print the result of 13 substracted from 22
+
+// Print the result of 22 multiplied by 13
+
+// Print the result of 22 divided by 13 (as a decimal fraction)
+
+// Print the remainder of 22 divided by 13
