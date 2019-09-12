7dea3b2d8dc2f03692a61740e8600305708098d3
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-12 12:08:39 +0200

    Adding greet others

--- week-01/day-4/workshop.ts
+++ week-01/day-4/workshop.ts
@@ -14,3 +14,9 @@ console.log('Humpty Dumpty sat on a wall,');
 console.log('Humpty Dumpty had a great fall.');
 console.log('All the king\'s horses and all the king\'s men');
 console.log('Couldn\'t put Humpty together again.');
+
+/* Greet others */
+
+console.log('Hello, Gabor!');
+console.log('Hello, Misi!');
+console.log('Hello, Kriszti!');
