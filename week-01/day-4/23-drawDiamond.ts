9d169722a62d398799b43afe0a9a60bc7c01bf07
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-13 08:10:12 +0200

    Cleaning up the code for the diamond

--- week-01/day-4/23-drawDiamond.ts
+++ week-01/day-4/23-drawDiamond.ts
@@ -1,8 +1,10 @@
 'use strict';
 
-let lineCount: number = 7;
-let starString: string = '*';
+let lineCount: number = 4;
+let star: string = '*';
+let space: string = ' ';
 let printString: string = '';
+let starString: string = '*';
 
 // Write a program that draws a
 // diamond like this:
@@ -16,19 +18,17 @@ let printString: string = '';
 //    *
 //
 // The diamond should have as many lines as lineCount is
-for (let i = 1; i <= Math.ceil(lineCount/2); i++){
+for (let i = 1; i <= lineCount; i++){
     printString = '';
-    for(let j = Math.ceil(lineCount/2) - i; j >= 1; j--){
-        printString += ' ';
+    for(let j = lineCount - i; j >= 1; j--){
+        printString += space;
     };
     console.log(printString+starString);
-    starString += '*';
-    starString += '*';
+    starString = starString + star + star;
 };
-starString = starString.slice(0,-2);
-printString = ' ';
-for(let k = 2; k <= Math.ceil(lineCount/2); k++){
-    starString = starString.slice(0,-2);
-    console.log(printString + starString);
-    printString += ' ';
+starString = starString.slice(0, -2);
+for(let k = lineCount -1; k > 0; k--){
+    printString += space;
+    starString = starString.slice(0, -2);
+    console.log(printString+starString);
 };
