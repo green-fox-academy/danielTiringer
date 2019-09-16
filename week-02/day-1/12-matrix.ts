3f072a65f638696b18d2f419275b2d3562693725
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-16 10:19:42 +0200

    Adding 01-third exercise

---
+++ week-02/day-1/12-matrix.ts
@@ -0,0 +1,26 @@
+// -  Create (dynamically*) a two dimensional list
+//    with the following matrix**. Use a loop!
+//
+//    0 0 0 1
+//    0 0 1 0
+//    0 1 0 0
+//    1 0 0 0
+//
+// -  Print this two dimensional list to the console
+//
+// * size should depend on a variable
+// ** Relax, a matrix is just like an array
+
+let matrixSize = 4
+let matrix:number[][];
+
+for (let i = 0; i < matrixSize; i++){
+    for (let j = 0; j < matrixSize; j++){
+        if (i = matrixSize - j){
+            matrix[i][j] = 1;
+        } else {
+            matrix[i][j] = 0;
+        };
+    };
+};
+console.log(matrix);
