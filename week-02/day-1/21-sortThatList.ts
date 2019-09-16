e3d398415004deec639b39dcddde046aad6e17f6
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-16 15:32:15 +0200

    Adding the list sorting exercise

--- week-02/day-1/21-sortThatList.ts
+++ week-02/day-1/21-sortThatList.ts
@@ -2,6 +2,22 @@
 //  Returns a list where the elements are sorted in ascending numerical order
 //  Make a second boolean parameter, if it's `true` sort that list descending
 
+let sorted: number[] = [];
+
+function bubble(array) {
+    array = array.sort((a, b) => a - b);
+    return array;
+};
+
+function advancedBubble(array, check) {
+    if (check === true) {
+        array = array.sort((a, b) => b - a);
+        return array;
+    } else {
+        return array;
+    }
+}
+
 //  Example:
 console.log(bubble([34, 12, 24, 9, 5]));
 //  should print [5, 9, 12, 24, 34]
