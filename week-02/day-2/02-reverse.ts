44e2d524dc714f5e8e7f12d18b7228f624a4a318
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-17 10:22:23 +0200

    Reversing the string

--- week-02/day-2/02-reverse.ts
+++ week-02/day-2/02-reverse.ts
@@ -3,6 +3,15 @@
 // Use it on this reversed string to check it!
 
 let reversed: string = `.eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI`;
+let newString: string = '';
+
+function reverse (string) {
+    for (let i = 0; i < string.length; i++){
+        newString += string.charAt(string.length - i -1);
+    }
+    return newString;
+}
+
 console.log(reverse(reversed));
 
 export = reverse;
