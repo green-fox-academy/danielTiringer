64aeccadc1d617bab5dc03815597f9da6a20fcc2
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-17 10:36:02 +0200

    Fixing the url

--- week-02/day-2/03-urlFixer.ts
+++ week-02/day-2/03-urlFixer.ts
@@ -3,5 +3,11 @@
 // Also, the URL is missing a crutial component, find out what it is and insert it too!
 
 let url: string = 'https//www.reddit.com/r/nevertellmethebots';
+let bad: string = "bots";
+let good: string = "odds"
+
+url = url.replace(bad, good);
+
+url = url.substring(0,5) + ':' + url.substring(5);
 
 console.log(url);
