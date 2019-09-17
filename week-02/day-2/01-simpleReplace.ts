2b66bb6ada2c07a7ce29a9b4ca777701ced73173
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-17 10:08:52 +0200

    Completing the simple replacement exercise'

--- week-02/day-2/01-simpleReplace.ts
+++ week-02/day-2/01-simpleReplace.ts
@@ -6,6 +6,11 @@ let example: string = 'In a dishwasher far far away';
 // Please fix it for me!
 // Expected ouput: In a galaxy far far away
 
+var toReplace = /dishwasher/;
+var replace: string = "galaxy";
+
+example = example.replace(toReplace, replace); 
+
 console.log(example);
 
 export = example;
