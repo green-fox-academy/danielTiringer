4f542299929f9506974bdc6a1a35912a345bebeb
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-18 11:10:32 +0200

    Square drawing around the center of the board, the fancy way

--- week-02/day-3/06-squareInTheCenter/squareInTheCenter.ts
+++ week-02/day-3/06-squareInTheCenter/squareInTheCenter.ts
@@ -6,3 +6,11 @@ const ctx = canvas.getContext('2d');
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 
 // Draw a green 10x10 square to the center of the canvas.
+
+function centerSquare (x, y){
+    ctx.beginPath();
+    ctx.strokeRect(x-5, y-5, 10, 10);
+
+};
+
+centerSquare(300, 200);
