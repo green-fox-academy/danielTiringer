81532c2b86ff1bcbb85011dab4cb9af4a4c04429
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-18 10:36:26 +0200

    Stoking lines through the middle

--- week-02/day-3/01-middleLines/middleLines.ts
+++ week-02/day-3/01-middleLines/middleLines.ts
@@ -6,5 +6,17 @@ const ctx = canvas.getContext('2d');
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 
 // draw a red horizontal line to the canvas' middle.
+
+ctx.beginPath();
+ctx.strokeStyle = 'red';
+ctx.moveTo(150, 200);
+ctx.lineTo(450, 200);
+ctx.stroke();
+
 // draw a green vertical line to the canvas' middle.
 
+ctx.beginPath();
+ctx.strokeStyle = 'green';
+ctx.moveTo(300,  50);
+ctx.lineTo(300, 350);
+ctx.stroke();
