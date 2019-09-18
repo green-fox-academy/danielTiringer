8d614d6ba6439893d47c70e95c81734a3394914e
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-18 13:16:40 +0200

    3D steps complete

---
+++ week-02/day-3/12-purpleSteps3D/purpleSteps3D.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Reproduce this:
+// [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/assets/r4.png]
+
+function purpleSquare (x, y) {
+    ctx.beginPath();
+    ctx.fillStyle = "magenta";
+    ctx.fillRect(x-y, x-y, y, y);
+    ctx.strokeRect(x-y, x-y, y, y);
+}
+
+let sum: number = 0;
+for(let i = 0; i < 11; i++){
+    sum += (i)*10;
+    purpleSquare(sum, i*10);
+}
