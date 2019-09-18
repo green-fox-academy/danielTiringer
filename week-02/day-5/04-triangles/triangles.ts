274e66ba8b470a3cc43d9907a7535f2212c012be
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-18 15:50:15 +0200

    Completing the triangles. Will fix up the height later on to make it mobile if I get the chance or the will...

---
+++ week-02/day-5/04-triangles/triangles.ts
@@ -0,0 +1,29 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+function triangle(a, b){
+    ctx.beginPath();
+    ctx.moveTo(a, b);
+    ctx.lineTo(a+side, b);
+    ctx.lineTo(a+side/2, b-17.32);
+    ctx.lineTo(a, b);
+    ctx.stroke();
+}
+
+let side: number = 20;
+let height: number = 17.32 // Math.sqrt(side^2-(side/2)^2)
+let startH: number = 50;
+let startV: number = 350;
+let currentV = startV;
+let currentH = startH;
+
+for(let j = 0; j < 20; j++){
+    
+    for (let i = 0; i < 20-j; i++){
+        triangle(currentH+i*side, currentV);
+    };
+    currentV -= height;
+    currentH += side/2;
+}
