5ec0fb9e9c7b67ae3a5d4a91cea326645ea9ae48
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-18 14:17:58 +0200

    Connectig all the dots

--- week-02/day-3/15-connectTheDots/connectTheDots.ts
+++ week-02/day-3/15-connectTheDots/connectTheDots.ts
@@ -13,7 +13,43 @@ const ctx = canvas.getContext('2d');
 // [120, 100], [85, 130], [50, 100]]
 
 function line(x, y){
-    ctx.beginPath();
-    ctx.moveTo(x, y);
+    ctx.strokeStyle = "green";
+    ctx.lineTo(x, y);
     ctx.stroke();
+}
+
+function restart(x, y){
+    ctx.beginPath();
+    ctx.strokeStyle = "green";
+    ctx.lineTo(x, y);
+    ctx.stroke();
+}
+
+let box: number [][] = [[10, 10], [290,  10], [290, 290], [10, 290]];
+
+for(let i = 0; i <= box.length; i++){
+    if(i<box.length){
+        let a = box[i][0];
+        let b = box[i][1];
+        line(a,b);
+    } else if (i = box.length) {
+        let a = box[0][0];
+        let b = box[0][1];
+        line(a,b);
+    }
+};
+
+
+let connect: number [][] = [[50, 100], [70, 70], [80, 90], [90, 90], [100, 70], [120, 100], [85, 130], [50, 100]];
+
+for(let i = 0; i <= connect.length; i++){
+    if(i == 0){
+        let a = connect[i][0];
+        let b = connect[i][1];
+        restart(a,b);
+    } else {
+        let a = connect[i][0];
+        let b = connect[i][1];
+        line(a,b);
+    }
 }
