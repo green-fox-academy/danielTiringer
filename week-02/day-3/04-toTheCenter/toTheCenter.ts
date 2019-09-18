8a985b042dcd3ec66b5e8595a3eaebcbf1ce1b18
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-18 11:02:36 +0200

    The lines to the center are functioning well

--- week-02/day-3/04-toTheCenter/toTheCenter.ts
+++ week-02/day-3/04-toTheCenter/toTheCenter.ts
@@ -9,3 +9,14 @@ const ctx = canvas.getContext('2d');
 // The x and y coordinates of the line's starting point
 // and draws a line from that point to the center of the canvas
 // Draw at least 3 lines with that function using a loop.
+
+function center (x, y) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(300, 200);
+    ctx.stroke();
+}
+
+center(100, 100);
+center(300, 300);
+center(550, 250);
