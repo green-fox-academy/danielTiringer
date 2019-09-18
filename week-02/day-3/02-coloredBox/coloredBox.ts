ab601cd8a337f66ebe82274ddfafb1fe872ad3a6
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-18 10:45:07 +0200

    Adding colorful lines

--- week-02/day-3/02-coloredBox/coloredBox.ts
+++ week-02/day-3/02-coloredBox/coloredBox.ts
@@ -6,3 +6,27 @@ const ctx = canvas.getContext('2d');
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 
 // draw a box that has different colored lines on each edge.
+
+ctx.beginPath();
+ctx.strokeStyle = 'green';
+ctx.moveTo(100,  50);
+ctx.lineTo(100, 350);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = 'purple';
+ctx.moveTo(100, 350);
+ctx.lineTo(500, 350);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = 'red';
+ctx.moveTo(500, 350);
+ctx.lineTo(500,  50);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = 'blue';
+ctx.moveTo(500,  50);
+ctx.lineTo(100,  50);
+ctx.stroke();
