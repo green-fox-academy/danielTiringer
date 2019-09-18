ae838591241095f2c7aafb72a5ab25ee49101a9e
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-18 17:18:27 +0200

    Rewriting the dot connection to half the lines. I made a single function and moved the drawing operation to within the for loop.

--- week-02/day-3/15-connectTheDots/connectTheDots.ts
+++ week-02/day-3/15-connectTheDots/connectTheDots.ts
@@ -12,44 +12,24 @@ const ctx = canvas.getContext('2d');
 // Connect these: [[50, 100], [70, 70], [80, 90], [90, 90], [100, 70],
 // [120, 100], [85, 130], [50, 100]]
 
-function line(x, y){
-    ctx.strokeStyle = "green";
-    ctx.lineTo(x, y);
-    ctx.stroke();
-}
-
-function restart(x, y){
-    ctx.beginPath();
-    ctx.strokeStyle = "green";
-    ctx.lineTo(x, y);
-    ctx.stroke();
-}
-
 let box: number [][] = [[10, 10], [290,  10], [290, 290], [10, 290]];
+let fox: number [][] = [[50, 100], [70, 70], [80, 90], [90, 90], [100, 70], [120, 100], [85, 130], [50, 100]];
 
-for(let i = 0; i <= box.length; i++){
-    if(i<box.length){
-        let a = box[i][0];
-        let b = box[i][1];
-        line(a,b);
-    } else if (i = box.length) {
-        let a = box[0][0];
-        let b = box[0][1];
-        line(a,b);
+
+function draw (array){
+    for(let i = 0; i <= array.length; i++){
+        if(i<array.length){
+            ctx.strokeStyle = "green";
+            ctx.lineTo(array[i][0], array[i][1]);
+            ctx.stroke();
+        } else if (i = array.length) {
+            ctx.strokeStyle = "green";
+            ctx.lineTo(array[0][0], array[0][1]);
+            ctx.stroke();
+        }
     }
+    ctx.beginPath();
 };
 
-
-let connect: number [][] = [[50, 100], [70, 70], [80, 90], [90, 90], [100, 70], [120, 100], [85, 130], [50, 100]];
-
-for(let i = 0; i <= connect.length; i++){
-    if(i == 0){
-        let a = connect[i][0];
-        let b = connect[i][1];
-        restart(a,b);
-    } else {
-        let a = connect[i][0];
-        let b = connect[i][1];
-        line(a,b);
-    }
-}
+draw(box);
+draw(fox);
