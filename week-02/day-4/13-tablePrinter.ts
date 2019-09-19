47313b8ba56879573d615a9fce962ca675a71f5a
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-19 15:52:33 +0200

    Printing tables

---
+++ week-02/day-4/13-tablePrinter.ts
@@ -0,0 +1,47 @@
+'use strict';
+// Create a function that prints the ingredient list of dictionaries to the console in the following format:
+//
+// +--------------------+---------------+----------+
+// | Ingredient         | Needs cooling | In stock |
+// +--------------------+---------------+----------+
+// | vodka              | Yes           | 1        |
+// | coffee_liqueur     | Yes           | -        |
+// | fresh_cream        | Yes           | 1        |
+// | captain_morgan_rum | Yes           | 2        |
+// | mint_leaves        | No            | -        |
+// +--------------------+---------------+----------+
+//
+// OPTIONAL
+// The frist columns should be automatically as wide as the longest key
+
+const ingredients: any[] = [
+	{ name: 'vodka', inStock: 1, needsCooling: true },
+	{ name: 'coffee_liqueur', inStock: 0, needsCooling: true },
+	{ name: 'fresh_cream', inStock: 1, needsCooling: true },
+	{ name: 'captain_morgan_rum', inStock: 2, needsCooling: true },
+	{ name: 'mint_leaves', inStock: 0, needsCooling: false },
+	{ name: 'sugar', inStock: 0, needsCooling: false },
+	{ name: 'lime juice', inStock: 0, needsCooling: true },
+	{ name: 'soda', inStock: 0, needsCooling: true }
+];
+checkIngredients(ingredients);
+
+function checkIngredients (array) {
+	let printArray: any [] = [];
+	let cooling: string;
+	for(let i: number = 0; i < ingredients.length; i++){
+		if(ingredients[i].needsCooling = true){
+			cooling = 'Yes'
+		} else {
+			cooling = 'No'
+		}
+		let object = {
+			'Ingredient': ingredients[i].name,
+			'Needs cooling': cooling,
+			'In stock': ingredients[i].inStock
+		}
+		printArray.push(object);
+	}
+
+	console.table(printArray);
+}
