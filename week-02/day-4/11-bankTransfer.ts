0880ce34972785447b64c481df8c1279320c3e17
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-19 13:43:08 +0200

    Bank transfers commencing

---
+++ week-02/day-4/11-bankTransfer.ts
@@ -0,0 +1,68 @@
+'use strict';
+
+const accounts: any[] = [
+  { clientName: 'Igor', accountNumber: 11234543, balance: 203004099.2 },
+  { clientName: 'Vladimir', accountNumber: 43546731, balance: 5204100071.23 },
+  { clientName: 'Sergei', accountNumber: 23456311, balance: 1353600.0 }
+];
+
+// Create function that returns the name and balance of cash on an account in a list
+// getNameAndBalance(11234543);
+// should return: ['Igor', 203004099.2]
+
+// Create function that transfers an amount of cash from one account to another
+// it should have four parameters:
+//  - the accounts
+//  - from accountNumber
+//  - to accountNumber
+//  - amount of cash to transfer
+//
+// Log "404 - account not found" if any of the account numbers don't exist to the console.
+
+// transferAmount(accounts, 43546731, 23456311, 500.0);
+//After printing the "accounts" it should look like:
+// const accounts = [
+//	{ clientName: 'Igor', accountNumber: 11234543, balance: 203004099.2 },
+//	{ clientName: 'Vladimir', accountNumber: 43546731, balance: 5204099571.23 },
+//	{ clientName: 'Sergei', accountNumber: 23456311, balance: 1354100.0 }
+//]
+
+let bankOperations = {
+    getNameAndBalance (number) {
+        for(let i = 0; i < accounts.length; i++){
+            if(accounts[i].accountNumber == number){
+            console.log(`'${accounts[i].clientName}', ${accounts[i].balance}`);
+            }
+        }
+    },
+    transferAmount (array, origin: number, target: number, amount: number) {
+        let allAccounts: number [] = [];
+        for(let j = 0; j < array.length; j++){
+            allAccounts.push(array[j].accountNumber)
+        };
+        for(let i = 0; i < array.length; i++){
+            if (allAccounts.indexOf(target) == -1 || allAccounts.indexOf(origin) == -1){
+                console.log('404 - account not found');
+            } else {
+                if (array[i].accountNumber == origin){
+                    array[i].balance = array[i].balance - amount;
+                    return array[i].balance;
+                }
+                if(array[i].accountNumber == target){
+                    array[i].balance = array[i].balance + amount;
+                    return array[i].balance;
+                };
+            };
+        };
+    },
+    readAccounts(array){
+        for (let i = 0; i < array.length; i++){
+            console.log(`clientName: '${accounts[i].clientName}', accountNumber: ${accounts[i].accountNumber}, balance: ${accounts[i].balance}`);
+        }
+        
+    }
+};
+
+bankOperations.getNameAndBalance(11234543);
+bankOperations.transferAmount(accounts, 43546731, 23456311, 500.0);
+bankOperations.readAccounts(accounts);
