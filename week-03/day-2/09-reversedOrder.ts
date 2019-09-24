e45dc44064da61bd8909c849174fe93fee23713d
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-24 15:00:14 +0200

    Decrypting the reversed order message

--- week-03/day-2/09-reversedOrder.ts
+++ week-03/day-2/09-reversedOrder.ts
@@ -1,1 +1,11 @@
 // Create a method that decrypts reversed-order.txt
+
+const fs = require('fs');
+
+function reverseLines (filePath: string): void {
+    let fileLines: string[] = fs.readFileSync(filePath, 'utf-8').toString().split('\n');
+    let decryptedMessage: string[] = [];
+    console.log(fileLines.reverse().join('\n'));
+};
+
+reverseLines('09-reversed-order.txt');
