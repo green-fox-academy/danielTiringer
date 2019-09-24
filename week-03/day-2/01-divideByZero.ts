cd1d180b7b12be1e1c905641307a9d34f850bf3b
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-24 13:04:09 +0200

    Adding alternatives to dividing zeros

--- week-03/day-2/01-divideByZero.ts
+++ week-03/day-2/01-divideByZero.ts
@@ -3,6 +3,8 @@
 // and prints the result.
 // It should print 'fail' if the parameter is 0
 
+// Option one, the old school way
+
 function divideTenBy (number: number): any {
     if(number == 0){
         console.log('fail');
@@ -13,3 +15,21 @@ function divideTenBy (number: number): any {
 
 divideTenBy(0);
 divideTenBy(90);
+
+// Option two, with try and catch
+
+function tryAndCatch (number: number): any {
+    try {
+        if(number === 0){
+            throw 'error';
+        } else {
+            console.log(10 / number)
+        }
+    } catch(error) {
+        console.log('fail');
+    }
+
+}
+
+tryAndCatch(0);
+tryAndCatch(90);
