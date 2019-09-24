f76b9b4f298c9018ecf67151718551484ab83313
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-24 14:46:47 +0200

    Decoding the cryptic message

--- week-03/day-2/07-doubled.ts
+++ week-03/day-2/07-doubled.ts
@@ -1,1 +1,30 @@
 // Create a method that decrypts duplicated-chars.txt
+
+
+const fs = require('fs');
+
+function decryptFile (filePath: string): void {
+    let fileLines: string[] = fs.readFileSync(filePath, 'utf-8').toString().split('\n');
+    let fileWords: string[] = [];
+    for (let i: number = 0; i < fileLines.length; i++){
+        let temporary: string[] = fileLines[i].split('  ');
+        for (let j: number = 0; j < temporary.length; j++){
+            fileWords.push(temporary[j]);
+        }
+        temporary = [];
+    }
+    let decodedMessage: string[] = [];
+    fileWords.forEach(function (word: string): void {
+        let storageString: string[] = word.split('');
+        let decryptedWord: string = '';
+        for(let i: number = 0; i < storageString.length; i++){
+            if(i % 2 == 0){
+                decryptedWord += storageString[i];
+            }
+        }
+        decodedMessage.push(decryptedWord);
+    })
+    console.log(decodedMessage.join(' '));
+};
+
+decryptFile('07-duplicated-chars.txt');
