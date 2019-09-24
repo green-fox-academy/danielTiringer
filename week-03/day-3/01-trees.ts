0f437c082a9bc73c7ca337a6173d35807328d118
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-24 07:56:03 +0200

    Adding homework

---
+++ week-03/day-3/01-trees.ts
@@ -0,0 +1,43 @@
+// Create 5 trees
+// Store the data of them in variables in your program
+// for every tree the program should store its'
+// type
+// leaf color
+// age
+// sex
+// you can use just variables, or lists and/or maps
+
+let pineTree = {
+    type: 'pine',
+    leafColor: 'silver',
+    age: 55,
+    sex: 'N/A'
+}
+
+let oakTree = {
+    type: 'oak',
+    leafColor: 'green',
+    age: '30',
+    sex: 'N/A'
+}
+
+let plumTree = {
+    type: 'plum',
+    leafColor: 'red',
+    age: '24',
+    sex: 'N/A'
+}
+
+let hickoryTree = {
+    type: 'hickory',
+    leafColor: 'green',
+    age: 61,
+    sex: 'N/A'
+}
+
+let agaveTree = {
+    type: 'agave',
+    leafColor: 'blue',
+    age: 12,
+    sex: 'N/A'
+}
