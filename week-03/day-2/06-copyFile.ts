6457524948b877d205f5b626eef46ae87ee318a2
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-24 13:40:02 +0200

    Copying files

--- week-03/day-2/06-copyFile.ts
+++ week-03/day-2/06-copyFile.ts
@@ -1,3 +1,13 @@
 // Write a function that copies the contents of a file into another
 // It should take the filenames as parameters
 // It should return a boolean that shows if the copy was successful
+
+const fs = require('fs');
+
+function copyFile (pathFrom: string, pathTo: string) {
+    let fileToCopy: string[] = fs.readFileSync(pathFrom, 'utf-8')
+    fs.writeFileSync(pathTo, fileToCopy);
+    console.log(true);
+}
+
+copyFile('05-multi-write.txt', '06-copy-file.txt');
