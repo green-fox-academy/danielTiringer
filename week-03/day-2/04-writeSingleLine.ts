cb2823f9ef8839836040d73ec51ef21e91c17115
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-24 13:10:04 +0200

    Completing the single line write

--- week-03/day-2/04-writeSingleLine.ts
+++ week-03/day-2/04-writeSingleLine.ts
@@ -3,3 +3,16 @@
 // The file should be named "my-file.txt"
 // In case the program is unable to write the file,
 // It should print the following error message: "Unable to write file: my-file.txt"
+
+const fs = require('fs');
+
+let myName: string = 'Daniel Tiringer'
+
+try{
+    fs.writeFileSync('04-my-file.txt', myName);
+//  Uncomment the line below to test the catch statement
+//    throw 'error';
+}
+catch (error){
+    console.log('Unable to write file: 04-my-file.txt')
+}
