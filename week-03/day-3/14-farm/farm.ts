995e9d6bd4954c16b514e8740c76f488f1049c5a
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-25 18:25:00 +0200

    Adding the animal farm

--- week-03/day-3/14-farm/farm.ts
+++ week-03/day-3/14-farm/farm.ts
@@ -4,3 +4,47 @@
 // it has slots which defines the number of free places for animals
 // breed() -> creates a new animal if there's place for it
 // slaughter() -> removes the least hungry animal
+
+import { Animal } from './../04-animals/animals';
+
+let listOfAnimals: Animal [] = [];
+
+listOfAnimals.push(new Animal);
+
+class Farm {
+    private _places: number;
+    private _list: Animal [];
+
+    public constructor (listOfAnimals: Animal []) {
+        this._list = listOfAnimals;
+        this._places = 10;
+    }
+
+    public breed (): void {
+        if(this._places - listOfAnimals.length > 0){
+            this._list.push(new Animal);
+        }
+    }
+
+    public slaughter (): void {
+        if(listOfAnimals.length > 0){
+            let lowestHunger: number;
+            let fullestAnimal: number;
+            for(let i: number = 0; i < listOfAnimals.length; i++){
+                if (i = 0){
+                    lowestHunger = listOfAnimals[i].hunger;
+                    fullestAnimal = i;
+                    console.log(lowestHunger);
+                    console.log(fullestAnimal);
+                }
+                if (listOfAnimals[i].hunger < lowestHunger){
+                    lowestHunger = listOfAnimals[i].hunger;
+                    fullestAnimal = i;
+                }
+            }
+            listOfAnimals.splice(fullestAnimal, 1);
+        }
+    }
+}
+
+let sunshineMeadows = new Farm (listOfAnimals);
