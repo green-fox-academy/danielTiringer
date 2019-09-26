eda0bf0a45602cc216691b5228f7de8cb02fa2be
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-26 12:27:11 +0200

    Adding the pirates with all their functions fully tested. They are ready to wage war. Onto the ships as

---
+++ week-03/day-3/16-pirates/pirate.ts
@@ -0,0 +1,95 @@
+export class Pirate {
+    private _name: string;
+    private _rumConsumed: number;
+    private _isPassedOut: boolean;
+    private _isDead: boolean;
+    private _hasParrot: boolean;
+
+    constructor(name) {
+        this._name = name;
+        this._rumConsumed = 0;
+        this._isDead = false;
+    }
+
+    public drinkSomeRum(): void {
+        if (this._isDead == true) {
+            console.log('The pirate is dead.')
+        } else {
+            this._rumConsumed += 1;
+        }
+    }
+
+    public howsItGoingMate(): void {
+        if (this._isDead == true){
+            console.log('The pirate is dead.')
+        } else if (this._isPassedOut == true) {
+            this._isPassedOut = false;
+            this._rumConsumed = 0;
+            console.log('Slept off ye ale, time methinks another!');
+        } else if (this._rumConsumed <= 4 && this._isDead == false) {
+            console.log('Pour me anudder!');
+        } else if (this._rumConsumed > 4 && this._isDead == false) {
+            console.log("Arghh, I'ma pirate. How d'ya d'ink its goin?");
+            this._isPassedOut = true;
+            console.log('/ The pirate passes out and sleeps it off /');
+        }
+    }
+
+    public die(): void {
+        this._isDead = true;
+    }
+
+    public brawl (otherPirate: Pirate): void {
+        let chance: number = Math.floor(Math.random()*3);
+        console.log(`${this._name} and ${otherPirate._name} started a brawl!`);
+        if (chance == 0){
+            this._isPassedOut = true;
+            otherPirate._isPassedOut = true;
+            console.log('Both pirates were worn out in the fight and passed out.')
+        } else if (chance == 1) {
+            this._isDead = true;
+            console.log(`${this._name} won the fight!`);
+        } else if (chance == 2) {
+            otherPirate.die();
+            console.log(`${otherPirate._name} won the fight!`)
+        }
+    }
+
+    public addParrot (): void {
+        if(this._hasParrot == false){
+            this._hasParrot = true;
+        }
+    }
+}
+
+/*
+The stuff below was used to test out the various functions written for the pirates.
+
+let scallyWag = new Pirate ('Scallywag');
+let scoundrel = new Pirate ('Scoundrel');
+let snytch = new Pirate ('Snytch');
+
+scallyWag.drinkSomeRum();
+scallyWag.drinkSomeRum();
+scallyWag.howsItGoingMate();
+scallyWag.drinkSomeRum();
+scallyWag.drinkSomeRum();
+scallyWag.drinkSomeRum();
+scallyWag.howsItGoingMate();
+scallyWag.howsItGoingMate();
+scallyWag.howsItGoingMate();
+
+scoundrel.brawl(snytch);
+
+*/
+// Create a Pirate class. While you can add other fields and methods, you must have these methods:-
+
+// drinkSomeRum() - intoxicates the Pirate some
+// howsItGoingMate() - when called, the Pirate replies, if drinkSomeRun was called:-
+// 0 to 4 times, "Pour me anudder!"
+// else, "Arghh, I'ma Pirate. How d'ya d'ink its goin?", the pirate passes out and sleeps it off.
+// If you manage to get this far, then you can try to do the following. - die() - this kills off the pirate, in which case, DrinkSomeRum, etc. just result in he's dead. - brawl(x) - where pirate fights another pirate (if that other pirate is alive) and there's a 1/3 chance, 1 dies, the other dies or they both pass out.
+
+// And... if you get that far...
+
+// Add a parrot.
