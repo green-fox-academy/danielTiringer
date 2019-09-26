96df84a0a50d36dd02ee42d81ec47610e5372b71
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-26 09:52:06 +0200

    Exporting the blogpost objects to use in the blog exercise

--- week-03/day-3/03-blogPost/blogPost.ts
+++ week-03/day-3/03-blogPost/blogPost.ts
@@ -12,7 +12,7 @@
 // Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.
 
 
-class BlogPost {
+export class BlogPost {
     private _authorName: string;
     private _title: string;
     private _text: string;
@@ -25,6 +25,8 @@ class BlogPost {
         this._publicationDate = publicationDate;
     }
 }
+
+export { loremIpsum, waitButWhy, superb }
 
 let loremIpsum = new BlogPost ('John Doe', 'Lorem Ipsum', '2000.05.04.', 'Lorem ipsum dolor sit amet.');
 let waitButWhy = new BlogPost ('Tim Urban', 'Wait but why', '2010.10.10.', 'A popular long-form, stick-figure-illustrated blog about almost everything.');
