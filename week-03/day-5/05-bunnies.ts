9c62e362344e1b66e994855dbfd1873f2c39c4af
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-27 10:33:20 +0200

    Completing the simple bunny ear calculator

--- week-03/day-5/05-bunnies.ts
+++ week-03/day-5/05-bunnies.ts
@@ -1,2 +1,12 @@
 // Bunnies
 // We have a number of bunnies and each bunny has two big floppy ears. We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+
+function bunnyEars (bunnies: number): number {
+    let ears: number = 0;
+    if (bunnies >= 1){
+        ears += 2 + bunnyEars(bunnies - 1);
+    }
+    return ears;
+}
+
+console.log(bunnyEars(80));
