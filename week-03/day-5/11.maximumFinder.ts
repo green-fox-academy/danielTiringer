0be735a3de9004d5bf8983e8518576d5c2217242
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-27 11:50:50 +0200

    Maximum is found with the recursion written. Not an elegant solution and needs some further thinking

--- week-03/day-5/11.maximumFinder.ts
+++ week-03/day-5/11.maximumFinder.ts
@@ -1,2 +1,19 @@
 // Maximum finder
 // Write a function that finds the largest element of an array using recursion.
+let myArray: number [] = [1, 5, 9, 5, 3, 4, 10, 5, 3, 2, 7]
+let largest: number;
+
+function findTheLargest (array: number [], index: number): number {
+    if (!largest){
+        largest = array[0];
+    }
+    if (index < array.length){
+        if(array[index] > largest){
+            largest = array[index];
+        }
+        findTheLargest(array, index + 1);
+    }
+    return largest;
+}
+
+console.log(findTheLargest(myArray, 0));
