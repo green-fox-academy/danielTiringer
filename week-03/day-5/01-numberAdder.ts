39d6238f36f32da0e72bf61b4df333f970f1d24f
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-27 10:03:08 +0200

    Recursive function written to add numbers from 1 to n

--- week-03/day-5/01-numberAdder.ts
+++ week-03/day-5/01-numberAdder.ts
@@ -1,1 +1,11 @@
 // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+
+function addN (n: number): number {
+    let sum: number = 0;
+    if (n > 0){
+        sum = n + addN(n - 1);
+    }
+    return sum;
+}
+
+console.log(addN(900));
