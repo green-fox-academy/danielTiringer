5eae8f7a110da9ae9b96513e559ca8442fca47f1
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-27 10:38:04 +0200

    Calculating the magical three eared bunnies

--- week-03/day-5/06-bunniesAgain.ts
+++ week-03/day-5/06-bunniesAgain.ts
@@ -1,2 +1,16 @@
 // Bunnies again
 // We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say have 3 ears, because they each have a raised foot. Recursively return the number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+function bunnyEarsAgain (bunnies: number): number {
+    let ears: number = 0;
+    if (bunnies >= 1){
+        if (bunnies % 2 == 0){
+            ears += 3 + bunnyEarsAgain(bunnies -1);
+        } else if (bunnies % 2 != 0){
+            ears += 2 + bunnyEarsAgain(bunnies - 1);
+        }
+    }
+    return ears;
+}
+
+console.log(bunnyEarsAgain(70));
