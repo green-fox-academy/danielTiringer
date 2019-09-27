b2acb1ff9ab0da2a3d62668684f728ad40e6abb8
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-27 10:18:31 +0200

    Tested the newly written power calculator with different numbers, works well

--- week-03/day-5/03-power.ts
+++ week-03/day-5/03-power.ts
@@ -1,2 +1,11 @@
 // Power
 // Given base and n that are both 1 or more, compute recursively (no loops) the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+
+function powerCalculator (base: number, power: number): number {
+    if (power > 1){
+        base *= powerCalculator(base, power - 1);
+    }
+    return base;
+}
+
+console.log(powerCalculator(2, 8));
