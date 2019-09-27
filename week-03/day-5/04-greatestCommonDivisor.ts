dda2cdf0aaa908483983690badf6897799732a1a
Daniel Tiringer <tiringerdaniel@gmail.com>
2019-09-27 10:31:00 +0200

    Completing the greatest common divisor

--- week-03/day-5/04-greatestCommonDivisor.ts
+++ week-03/day-5/04-greatestCommonDivisor.ts
@@ -1,2 +1,11 @@
 // Greatest Common Divisor
 // Find the greatest common divisor of two numbers using recursion.
+
+function greatestCommonDivisor (numberA, numberB) {
+    if (!numberB) {
+        return numberA;
+    }
+    return greatestCommonDivisor(numberB, numberA % numberB);
+}
+
+console.log(greatestCommonDivisor(99, 3988));
