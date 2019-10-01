e61b9352eed1d10d410f907dd8aa4b4ea0725566
danielTiringer <tiringerdaniel@gmail.com>
2019-10-01 13:51:24 +0200

    Implementing the flyable property for the birds

--- week-04/day-2/02-animalsInTheZoo/bird.ts
+++ week-04/day-2/02-animalsInTheZoo/bird.ts
@@ -2,12 +2,22 @@
 
 import { Animal } from './animal';
 import { EggLayer } from './animal'
+import { Flyable } from '../03-flyable/flyable';
 
-export class Bird extends Animal implements EggLayer {
+export class Bird extends Animal implements EggLayer, Flyable {
     public constructor (name: string) {
         super(name);
     }
     public breed(): string {
         return 'laying eggs.';
+    }
+    public land(): void {
+        console.log('The bird lands on the tree.')
+    }
+    public takeOff(): void {
+        console.log('The bird pushes off the branch and takes flight!')
+    }
+    public fly(): void{
+        console.log('The bird flies high in the sky!')
     }
 }
