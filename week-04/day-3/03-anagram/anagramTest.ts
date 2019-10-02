74acfae8e0f13be2c8748bb4812defdb25bb63d5
danielTiringer <tiringerdaniel@gmail.com>
2019-10-02 12:08:19 +0200

    Adding a thrid argument to the evaluation function to provide a more meaningful message

--- week-04/day-3/03-anagram/anagramTest.ts
+++ week-04/day-3/03-anagram/anagramTest.ts
@@ -9,19 +9,19 @@ test('Anagram checker function', t => {
     let testOneStringB: string = 'siLEnt';
     const actualTestOne = anagramChecker(testOneStringA, testOneStringB); 
     const expectedTestOne = true;
-    t.equal(actualTestOne, expectedTestOne);
+    t.equal(actualTestOne, expectedTestOne, `${testOneStringA} equals ${testOneStringB}`);
 
     let testTwoStringA: string = 'integral';
     let testTwoStringB: string = 'Triangle';
     const actualTestTwo = anagramChecker(testTwoStringA, testTwoStringB); 
     const expectedTestTwo = true;
-    t.equal(actualTestTwo, expectedTestTwo);
+    t.equal(actualTestTwo, expectedTestTwo, `${testTwoStringA} equals ${testTwoStringB}`);
 
     let testThreeStringA: string = 'kitten';
     let testThreeStringB: string = 'hamster';
     const actualTestThree = anagramChecker(testThreeStringA, testThreeStringB); 
     const expectedTestThree = false;
-    t.equal(actualTestThree, expectedTestThree);
+    t.equal(actualTestThree, expectedTestThree, `${testThreeStringA} doesn't equal ${testThreeStringB}`);
     
     t.end();
 });
