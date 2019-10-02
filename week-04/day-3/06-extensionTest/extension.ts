d3d0b21ffe68d9064f4dd08884591d5694076450
danielTiringer <tiringerdaniel@gmail.com>
2019-10-02 13:21:18 +0200

    Completing the extensions

--- week-04/day-3/06-extensionTest/extension.ts
+++ week-04/day-3/06-extensionTest/extension.ts
@@ -1,23 +1,34 @@
+// Check out the folder. There's a work file and a test file.
+
+//  -  Run the tests, all 10 should be green (passing).
+//  -  The implementations though are not quite good.
+//  -  Create tests that'll fail, and will show how the implementations are wrong(You can assume that the implementation of join and split are good)
+//  -  After creating the tests, fix the implementations
+//  -  Check again, if you can create failing tests
+//  -  Implement if needed
+
 'use strict';
 
 export function add(a: number, b: number): number {
-  return 5;
+  return a + b;
 }
 
 export function maxOfThree(a: number, b: number, c: number): number {
-  if (a > b) {
+  if (a > b && a > c) {
     return a;
-  } else {
+  } else if (b > a && b > c) {
+    return b;
+  } else if (c > a && c > b) {
     return c;
   }
 };
 
 export function median(pool: number[]): number {
-  return pool[Math.floor((pool.length - 1) / 2)];
+  return Math.floor((pool.reduce((accumulator, currentValue) => accumulator + currentValue))/pool.length);
 }
 
 export function isVowel(character: string): boolean {
-  return ['a', 'u', 'o', 'e', 'i'].some(vowel => vowel === character);
+  return ['a', 'u', 'o', 'e', 'i'].some(vowel => vowel === character.toLowerCase());
 }
 
 export function translate(hungarian: string): string {
@@ -34,12 +45,3 @@ export function translate(hungarian: string): string {
   }
   return teve;
 }
-
-// Check out the folder. There's a work file and a test file.
-
-//  -  Run the tests, all 10 should be green (passing).
-//  -  The implementations though are not quite good.
-//  -  Create tests that'll fail, and will show how the implementations are wrong(You can assume that the implementation of join and split are good)
-//  -  After creating the tests, fix the implementations
-//  -  Check again, if you can create failing tests
-//  -  Implement if needed
--- week-04/day-3/06-extensionTest/test.ts
+++ week-04/day-3/06-extensionTest/test.ts
@@ -1,60 +1,84 @@
+// Check out the folder. There's a work file and a test file.
+
+//  -  Run the tests, all 10 should be green (passing).
+//  -  The implementations though are not quite good.
+//  -  Create tests that'll fail, and will show how the implementations are wrong(You can assume that the implementation of join and split are good)
+//  -  After creating the tests, fix the implementations
+//  -  Check again, if you can create failing tests
+//  -  Implement if needed
+
 'use strict';
 
-import { test } from 'tape';
+import * as test from 'tape';
 import { add, maxOfThree, median, isVowel, translate } from './extension';
 
+// 1
 test('add: 2 and 3 is 5', function (t: any): any {
   t.equal(add(2, 3), 5);
   t.end();
 });
 
+// 2
 test('add: 1 and 4 is 5', function (t: any): any {
-  t.equal(add(1, 4), 5);
+  t.notEqual(add(1, 4), 9);
   t.end();
 });
 
+// 3
 test('max of three: first', function (t: any): any {
   t.equal(maxOfThree(5, 4, 3), 5);
   t.end();
 });
 
+// 4
 test('max of three: third', function (t: any): any {
   t.equal(maxOfThree(3, 4, 5), 5);
   t.end();
 });
 
+// 5
 test('median: four', function (t: any): any {
   t.equal(median([7, 5, 3, 5]), 5);
   t.end();
 });
 
+// 6
 test('median: five', function (t: any): any {
   t.equal(median([1, 2, 3, 4, 5]), 3);
   t.end();
 });
 
+// 7
 test('is vowel: a', function (t: any): any {
   t.ok(isVowel('a'));
   t.end();
 });
 
+// 8
 test('is vowel: u', function (t: any): any {
-  t.ok(isVowel('u'));
+  t.ok(isVowel('U'));
   t.end();
 });
 
+// 9
 test('is vowel: k', function(t: any): any {
   t.notOk(isVowel('k'));
   t.end();
 });
 
+// 10
 test('translate: bemutatkozik', function (t: any): any {
   t.equal(translate('bemutatkozik'), 'bevemuvutavatkovozivik');
   t.end();
 });
 
+// 11
 test('translate: lagopus', function (t: any): any {
   t.equal(translate('lagopus'), 'lavagovopuvus');
   t.end();
 });
-© 2019 GitHub, Inc.
+
+test('translate: sanyi', function (t: any): any {
+  t.equal(translate('sanyi'), 'savanyivi');
+  t.end();
+});
