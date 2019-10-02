80bb26e024a43a825e3bb1ec9fb655fd3d0e686a
danielTiringer <tiringerdaniel@gmail.com>
2019-10-02 23:40:49 +0200

    All adjacent characters are now separated with a *.

--- week-03/day-5/09-stringsAgainAndAgain.ts
+++ week-03/day-5/09-stringsAgainAndAgain.ts
@@ -1,2 +1,57 @@
 // Strings again and again
 // Given a string, compute recursively a new string where all the adjacent chars are now separated by a *
+
+'use strict'
+
+let myString: string = 'The black cat climbed on the roof.';
+
+let stringArray: string[] = [];
+
+for (let i: number = 0; i < myString.split('').length; i++){
+	stringArray.push(myString.split('')[i]);
+   if(myString.split('')[i+1] != ' ' && myString.split('')[i] != ' ' && myString.split('')[i] != '.' && myString.split('')[i+1] != '.'){
+	stringArray.push('*');
+   }
+
+}
+console.log(stringArray.join(''));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
