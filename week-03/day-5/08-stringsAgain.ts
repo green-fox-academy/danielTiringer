78fe2cd7b4b366462db4ec6ad756e4e577a7928c
danielTiringer <tiringerdaniel@gmail.com>
2019-10-04 17:02:12 +0200

    Removing obsolete folders

--- week-03/day-5/08-stringsAgain.ts
+++ week-03/day-5/08-stringsAgain.ts
@@ -1,2 +1,40 @@
 // Strings again
 // Given a string, compute recursively a new string where all the 'x' chars have been removed.
+
+'use strict'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
