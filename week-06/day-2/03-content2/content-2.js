2a69c020b8090706475298e779effa714ccf1431
danielTiringer <tiringerdaniel@gmail.com>
2019-10-22 13:31:40 +0200

    Completing the content 2 exercise

---
+++ week-06/day-2/03-content2/content-2.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// Task one
+
+let newItemsList = ['apple', 'banana', 'cat', 'dog'];
+let listItems = document.querySelectorAll('li');
+console.log(listItems);
+newItemsList.forEach(element => {
+	listItems[newItemsList.indexOf(element)].innerHTML = element
+});
+
+// Task two
+
+let unorderedList = document.querySelector('ul');
+unorderedList.setAttribute('class', 'lime')
+console.log(unorderedList);
+
