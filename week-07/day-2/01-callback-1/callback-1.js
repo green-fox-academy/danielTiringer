592af75461cdc0c627b80c062f290f1eafb8c72a
danielTiringer <tiringerdaniel@gmail.com>
2019-10-28 20:19:43 +0100

    Cleaned up with nicer function

--- week-07/day-2/01-callback-1/callback-1.js
+++ week-07/day-2/01-callback-1/callback-1.js
@@ -3,12 +3,13 @@
 const mapWith = (array, callback) => {
   let output = [];
 
-	for (let i = 0; i < array.length; i++) {
-		output.push(callback(array[i]));
-	}
   // The mapWith() function should iterate over the given array and call the callback function on every element.
   // It stores the callback return values in the output.
   // The mapWith() should return with the output array.
+
+	array.forEach(element => {
+		output.push(callback(element))
+	});
 
   return output;
 }
