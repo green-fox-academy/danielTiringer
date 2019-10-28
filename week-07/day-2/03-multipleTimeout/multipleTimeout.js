f7b0ce617bbcb0ac03603b44d0420ec347ef0784
danielTiringer <tiringerdaniel@gmail.com>
2019-10-28 20:39:51 +0100

    Completing the multiple timeouts

--- week-07/day-2/03-multipleTimeout/multipleTimeout.js
+++ week-07/day-2/03-multipleTimeout/multipleTimeout.js
@@ -5,3 +5,17 @@
 // pear -> after 1 seconds
 // melon -> after 3 seconds
 // grapes -> after 5 seconds
+
+const printFruits = () => {
+	console.log('apple');
+  setTimeout(() => {
+    console.log('pear');
+  }, 1000);
+  setTimeout(() => {
+    console.log('melon');
+  }, 3000);
+  setTimeout(() => {
+    console.log('grapes');
+  }, 5000);
+};
+printFruits();
