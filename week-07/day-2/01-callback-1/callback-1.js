105964615c8a29982add31ec8d10cd5d8427cc0c
danielTiringer <tiringerdaniel@gmail.com>
2019-10-28 20:17:56 +0100

    Part 1 of the exercise is functional

--- week-07/day-2/01-callback-1/callback-1.js
+++ week-07/day-2/01-callback-1/callback-1.js
@@ -3,6 +3,9 @@
 const mapWith = (array, callback) => {
   let output = [];
 
+	for (let i = 0; i < array.length; i++) {
+		output.push(callback(array[i]));
+	}
   // The mapWith() function should iterate over the given array and call the callback function on every element.
   // It stores the callback return values in the output.
   // The mapWith() should return with the output array.
@@ -25,5 +28,5 @@ console.log(mapWith([1, 2, 3], addOne));
 
 const words = ['map', 'reduce', 'filter'];
 
-console.log(mapWith(words, removeSecondLetter));
+// console.log(mapWith(words, removeSecondLetter));
 // expected result: ['mp','rdc', 'fle']
