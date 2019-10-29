726d3ba71a4be160dfd2200c83bf07a8fa97b785
danielTiringer <tiringerdaniel@gmail.com>
2019-10-29 13:48:38 +0100

    Completing the first five parts of the giphy API

---
+++ week-07/day-2/07-giphyAPI/giphyAPI.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// Display gifs of a cute/funny topic using: https://developers.giphy.com/docs/
+// Obtain an API key
+// let apiKey = 1rzC0Cx7aJLeAzJdOMKcGDtmLUlsTpE2;
+// Search/Find the images in the API
+let giphyRequest = new XMLHttpRequest();
+
+giphyRequest.open('GET', "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=1rzC0Cx7aJLeAzJdOMKcGDtmLUlsTpE2&limit=20", true);
+
+let section = document.querySelector('section');
+
+giphyRequest.onload = function () {
+	let giphyData = JSON.parse(giphyRequest.responseText);
+	console.log(giphyData.data[0].images);
+	for (let i = 0; i < 16; i++){
+		let newImage = document.createElement('img');
+		newImage.setAttribute('src', giphyData.data[i].images.fixed_height_small_still.url)
+		section.appendChild(newImage);
+	}
+}
+giphyRequest.send();
+
+// Display the list of the first 16 results's static thumbnail
+// If the user clicks on the thumbnail, display the animated GIF
+
+let images = document.querySelectorAll('img');
+images.onclick = () => {
+	images.setAttribute('src','');
+}
