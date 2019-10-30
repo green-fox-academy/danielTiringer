76078e9e16239f7f3fce602298a1d8be5694fd32
danielTiringer <tiringerdaniel@gmail.com>
2019-10-30 08:50:43 +0100

    Adding ejs, completing the doubling exercise

--- week-07/day-3/01-frontEnd/app.js
+++ week-07/day-3/01-frontEnd/app.js
@@ -1,18 +1,23 @@
 'use strict';
 
 const express = require('express');
+const doubling = require('./doubling');
 const app = express();
-const PORT = 3000;
-
-// start express app on port 3000
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+const PORT = 8080;
 
 app.use(express.static('assets'));
+app.use(express.json());
 const path = require('path');
 
 // home page
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// doubling endpoint
+app.get('/doubling', doubling);
+
+// start express app on port 8080
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
---
+++ week-07/day-3/01-frontEnd/doubling.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const doubling = (req, res) => {
+    let doublingOutput = {};
+    if (!isNaN(req.query.input)) {
+        doublingOutput = {
+            "received": req.query.input,
+            "result": req.query.input * 2
+        };
+    } else {
+        doublingOutput = {
+            "error": "Please provide an input!"
+        };
+    }
+    res.send(doublingOutput);
+}
+
+module.exports = doubling;
