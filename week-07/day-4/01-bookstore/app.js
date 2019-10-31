d89c670823905019d9b100726a165d6d2c7ec457
danielTiringer <tiringerdaniel@gmail.com>
2019-10-31 12:53:49 +0100

    The template renders well with ejs, needs to be refactored to JSON

--- week-07/day-4/01-bookstore/app.js
+++ week-07/day-4/01-bookstore/app.js
@@ -1,43 +1,56 @@
 'use strict';
 
-const mysql = require('mysql');
-const dotenv = require('dotenv').config();
 const express = require('express');
-const database = require('data');
+const mysql = require('mysql');
+const env = require('dotenv').config();
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('assets'));
 app.use(express.json());
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
 
-db.connect({
-})
-
+// Inport the parameters of the MySQL database
 let conn = mysql.createConnection ({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: 'data',
+  database: process.env.DB_NAME,
 });
 
+// Connect the MySQL database
 conn.connect(function(err) {
   if (err) {
+		console.error(err);
     console.log('Error connecting to Db');
     return;
   }
   console.log('Connection established');
 });
 
-conn.end();
-
+// Send in the MySQL query
 app.get('/', function(req, res) {
-  conn.query('SELECT book_name FROM book_mast;', function(err, rows) {
+  conn.query('\
+		SELECT book_name,\
+		aut_name,\
+		cate_descrip,\
+		pub_name,\
+		book_price\
+		FROM book_mast\
+		INNER JOIN author ON book_mast.aut_id  = author.aut_id\
+		INNER JOIN category on category.cate_id = book_mast.cate_id\
+		INNER JOIN publisher on publisher.pub_id = book_mast.pub_id\
+		;', function(err, rows) {
     if (err) {
       console.log(err.toString());
       res.status(500).send('Database error');
       return;
     }
-    res.send(rows);
+		// let queryRows = rows;
+    // res.send(queryRows);
+		res.render('home', {
+			rows: rows
+		})
   });
 });
 
@@ -46,3 +59,4 @@ app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 
+// conn.end();
