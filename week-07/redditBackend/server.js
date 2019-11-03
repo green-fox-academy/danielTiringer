44e37f63bc7f6ed872a604ad043f556407dbaad4
danielTiringer <tiringerdaniel@gmail.com>
2019-11-03 03:53:26 +0100

    Creating the basic hello endpoint to verify server operation. Creating the initially planned SQL tables for the project.

---
+++ week-07/redditBackend/server.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const express = require('express');
+const mysql = require('mysql');
+const env = require('dotenv').config();
+const createSqlTable = require('./createSqlTable');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// Import the parameters of the MySQL database
+let conn = mysql.createConnection ({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+// Connect the MySQL database
+conn.connect(function(err) {
+  err ? console.log('Error connecting to the database.') : console.log('Connection established.');
+});
+
+// Render a Hello world for verification
+app.get('/hello/', (req, res) => {
+	res.send('Hello World!');
+});
+
+// Create the MySQL tables
+// conn.query(createSqlTable.createPostsSqlTable, function(err, res) {
+// 	err ? console.log('Unable to create the new table.') : console.log('The table is ready.');
+// });
+
+// conn.query(createSqlTable.createVotesSqlTable, function(err, res) {
+// 	err ? console.log('Unable to create the new table.') : console.log('The table is ready.');
+// });
+
+// conn.query(createSqlTable.createUsersSqlTable, function(err, res) {
+// 	err ? console.log('Unable to create the new table.') : console.log('The table is ready.');
+// });
+
+// start express app on port 3000
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
