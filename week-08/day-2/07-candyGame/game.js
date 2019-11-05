6a71fb4aa529d31c58a664c8a6755a23335f4bd1
danielTiringer <tiringerdaniel@gmail.com>
2019-11-05 11:30:28 +0100

    Completing the candy counter, candy button, lolly counter and lolly button functions. The candies per second is a work in progress

---
+++ week-08/day-2/07-candyGame/game.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// - Gather 10.000 candies!
+// - Clicking the ‘Create candies’ button gives you 1 candy.
+// - You can buy a lollipop for 100 candies by clicking the ‘Buy lollipop’ button.
+// - 10 lollipops generate 1 candy per second.
+//   - Use the 🍭 emoji to display the lollipops you have
+// - Display the candy producton rate in the `Candies / Second` row
+// - If you press the "make candy rain" button, the candy generation should speed up 10x
+
+let candyCounter = document.querySelector('.candies');
+let lollyCounter = document.querySelector('.lollypops');
+let candiesPerSecondCounter = document.querySelector('.speed');
+
+let candyButton = document.querySelector('.create-candies');
+let lollyButton = document.querySelector('.buy-lollypops');
+let machineButton = document.querySelector('.candy-machine');
+
+let lollypop = lollyCounter.textContent.slice(0, 2);
+
+candyButton.onclick = () => {
+	candyCounter.textContent++;
+};
+
+lollyButton.onclick = () => {
+	candyCounter.textContent >= 100 ?
+		(
+			(candyCounter.textContent = candyCounter.textContent - 100)
+			&& (lollyCounter.textContent = lollyCounter.textContent + lollypop)
+		)
+	: undefined;
+};
+
+machineButton.onclick = () => {
+	candiesPerSecondCounter.textContent = candiesPerSecondCounter.textContent * 10
+};
