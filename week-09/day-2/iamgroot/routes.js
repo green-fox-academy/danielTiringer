35d3d6a8b6a93cea57ea4668345a51922890d055
danielTiringer <tiringerdaniel@gmail.com>
2019-11-12 17:18:13 +0100

    Completing the yondu arrow speed counter

--- week-09/day-2/iamgroot/routes.js
+++ week-09/day-2/iamgroot/routes.js
@@ -2,9 +2,11 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
 app.get('/groot', (req, res) => {
-  // TODO: implement this method
+
 	let response = {};
 	response.received = req.query.message;
 	response.translated = 'I am Groot!';
@@ -15,6 +17,26 @@ app.get('/groot', (req, res) => {
 	:
 		res.status(400) &&
 		res.json( {error: 'No message to translate'} );
+
 });
+
+app.get('/yondu', (req, res) => {
+
+	if (req.query.distance > 0 && req.query.time > 0) {
+		let response = {}
+		response.distance = parseInt(req.query.distance);
+		response.time = parseInt(req.query.time);
+		response.speed = response.distance / response.time;
+
+		res.status(200);
+		res.json(response);
+
+	} else if (req.query.time <= 0) {
+		res.status(400);
+		res.json( { error: 'Invalid time value.' } );
+	}
+
+});
+
 
 module.exports = app;
--- week-09/day-2/iamgroot/test/test-routes.js
+++ week-09/day-2/iamgroot/test/test-routes.js
@@ -4,7 +4,7 @@ const test = require('tape');
 const request = require('supertest');
 const app = require('../routes');
 
-test('groot endpoint', (t) => {
+test('Groot endpoint', (t) => {
 	request(app)
     .get('/groot')
 		.query( { message: 'somemessage' } )
@@ -14,11 +14,7 @@ test('groot endpoint', (t) => {
 			let expectedResponse = { received: 'somemessage', translated: 'I am Groot!'  }
       t.error(err, 'No error');
       t.same(res.body, expectedResponse, 'Correct response from Groot');
-      t.end();
     });
-});
-
-test('groot endpoint without message', (t) => {
 	request(app)
     .get('/groot')
 		.query( {} )
@@ -26,6 +22,30 @@ test('groot endpoint without message', (t) => {
     .expect(400)
     .end(function (err, res) {
 			let expectedResponse = { error: 'No message to translate' }
+      t.error(err, 'No error');
+      t.same(res.body, expectedResponse, 'Correct error message');
+      t.end();
+    });
+});
+
+test('Yondu endpoint', (t) => {
+	request(app)
+    .get('/yondu')
+		.query( { distance: 100.0, time: 10.0 } )
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end(function (err, res) {
+			let expectedResponse = { distance: 100.0, time: 10.0, speed: 10.0 }
+      t.error(err, 'No error');
+      t.same(res.body, expectedResponse, 'Correctly calculating the speed of the arrow.');
+    });
+	request(app)
+    .get('/yondu')
+		.query( { distance: 100.0, time: -1.0 } )
+    .expect('Content-Type', /json/)
+    .expect(400)
+    .end(function (err, res) {
+			let expectedResponse = { error: 'Invalid time value.' }
       t.error(err, 'No error');
       t.same(res.body, expectedResponse, 'Correct error message');
       t.end();
