3a4442aec1b13b5f19ddc29975094bc1b10b44b1
danielTiringer <tiringerdaniel@gmail.com>
2019-11-21 16:27:59 +0100

    fix(editpost): modifying how the endpoint works to be more elegant

--- week-09/redditFullStack/views/editpost.js
+++ week-09/redditFullStack/views/editpost.js
@@ -1,11 +1,14 @@
 'use strict';
 
 // Populating the title and the url fields with the data from the original post
+let form = document.querySelector('.new-post-form');
 let titleField = document.querySelector('#title');
 let urlField = document.querySelector('#url');
 let postIdField = document.querySelector('#post-id');
 
 let postIdValue = window.location.href.split('=')[1];
+
+form.action = `/posts/${postIdValue}`;
 
 const getPostData = (postId) => {
 	fetch('http://localhost:3000/posts', {
@@ -18,7 +21,6 @@ const getPostData = (postId) => {
     .then(result => {
 			result.filter(post => {
 				if (post.post_id == postId) {
-					console.log(post);
 					postIdField.value = postIdValue;
 					urlField.value = post.url;
 					titleField.textContent = post.title;
