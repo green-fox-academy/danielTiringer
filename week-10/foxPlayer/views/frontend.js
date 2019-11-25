efa89d9db091cf5480929b2cde4d95b0615fa3c9
danielTiringer <tiringerdaniel@gmail.com>
2019-11-25 13:41:00 +0100

    feat(timedisplay): elapsed time and total track length are displayed on the bottom control bar

--- week-10/foxPlayer/views/frontend.js
+++ week-10/foxPlayer/views/frontend.js
@@ -2,7 +2,9 @@
 
 let track01 = document.querySelector('#track-01');
 
-let playPauseButton = document.querySelector('.play-pause');
+const playPauseButton = document.querySelector('.play-pause');
+const trackElapsedTimeDisplay = document.querySelector('#elapsed-time');
+const trackLengthDisplay = document.querySelector('#total-length');
 
 playPauseButton.addEventListener('click', (event) => {
 	console.log('You clicked on the pause/play button');
@@ -10,6 +12,38 @@ playPauseButton.addEventListener('click', (event) => {
 	togglePlayPause();
 });
 
+track01.addEventListener('loadstart', (event) => {
+	console.log(event);
+});
+
+track01.addEventListener('play', (event) => {
+	console.log(event);
+	console.log(event.target.currentTime);
+});
+
+track01.addEventListener('ended', (event) => {
+	console.log(event);
+});
+
+track01.addEventListener('progress', (event) => {
+	console.log(event);
+	console.log(event.target.duration);
+	console.log(durationConverter(event.target.duration));
+	trackLengthDisplay.textContent = durationConverter(event.target.duration);
+});
+
+track01.ontimeupdate = function () {
+	console.log(track01.currentTime);
+	trackElapsedTimeDisplay.textContent = durationConverter(track01.currentTime);
+}
+
+const durationConverter = (duration) => {
+	let hours = Math.floor(duration / 3600);
+	let minutes = Math.floor(duration / 60);
+	let seconds = Math.round(duration % 60);
+
+	return hours > 0 ? `${hours}:${minutes}:${seconds}` : `${minutes}:${seconds}`
+};
 
 const togglePlayPause = () => {
 	if (playPauseButton.getAttribute('id') == 'play') {
